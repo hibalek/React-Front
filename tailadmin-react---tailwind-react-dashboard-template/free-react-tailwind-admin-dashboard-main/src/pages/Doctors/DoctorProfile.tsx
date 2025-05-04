63717ba7fb303801481590875f668722bd4f629b
ahlem adrar <ahlem.adrar@univ-constantine2.dz>
2025-05-04 15:32:07 +0200

    Update DoctorProfile.tsx

--- tailadmin-react---tailwind-react-dashboard-template/free-react-tailwind-admin-dashboard-main/src/pages/Doctors/DoctorProfile.tsx
+++ tailadmin-react---tailwind-react-dashboard-template/free-react-tailwind-admin-dashboard-main/src/pages/Doctors/DoctorProfile.tsx
@@ -495,7 +495,7 @@ const Sidebar = React.memo(({
 // Composant Principal
 const DoctorProfile = () => {
   const [doctor, setDoctor] = useState<DoctorProfileData>({
-    fullName: "Dr. Adrar Ahlem Lemia",
+    fullName: "Dr. Adrar Ahlem Lemia lwahraniya ",
     specialty: "Nutritionniste & Diététicienne",
     profileImage: "/doctor-avatar.jpg",
     bannerImage: "/doctor-banner.jpg",
